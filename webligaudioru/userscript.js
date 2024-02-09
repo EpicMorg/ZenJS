206804c72400b0b33630e17a283bc0129040a7a5
STAM <stamepicmorg@users.noreply.github.com>
2024-02-09 21:59:10 +0300

    2024020902 userscript.js

--- webligaudioru/userscript.js
+++ webligaudioru/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         web.ligaudio.ru Safe dowloader
 // @namespace    epicmorg.ligaudioru.downloader
-// @version      2024020901
+// @version      2024020902
 // @description  Download files with safe without annoyng ads
 // @license      MIT
 // @author       EpicMorg DevTeam
@@ -9,6 +9,8 @@
 // @match        *://web.ligaudio.ru/*
 // @match        *://web.ligaudio.ru/mp3/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ligaudio.ru
+// @downloadURL  https://raw.githack.com/EpicMorg/ZenJS/master/webligaudioru/userscript.js
+// @updateURL    https://raw.githack.com/EpicMorg/ZenJS/master/webligaudioru/userscript.js
 // @grant        none
 // ==/UserScript==
 
